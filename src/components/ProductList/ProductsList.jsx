6f94cde4bad5ae592ac1da87849945c6b6af3ff0
YevheniiGrabina <89870811+YevheniiGrabina@users.noreply.github.com>
2023-12-23 15:20:35 +0200

    fix productlist

--- src/components/ProductList/ProductsList.jsx
+++ src/components/ProductList/ProductsList.jsx
@@ -1,54 +1,116 @@
 import React, { useState } from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Products from "../Products/Products"
 import  css from'./ProductList.module.css'
 
 const ProductsList = ({ items, addToCart }) => {
   const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedVariant, setSelectedVariant] = useState(null);
+  const [variantModalOpen, setVariantModalOpen] = useState(false);
 
   const openModal = (item) => {
     setSelectedItem(item);
+    setSelectedVariant(item.variants ? item.variants[0] : null);
+    setVariantModalOpen(false); // Скрыть окно с вариантом при открытии основного окна
   };
 
   const closeModal = () => {
     setSelectedItem(null);
+    setSelectedVariant(null);
+    setVariantModalOpen(false);
   };
 
- const handleAddToCart = (item) => {
-  addToCart(item);
-   closeModal();
-};
+  const handleAddToCart = () => {
+    if (selectedItem) {
+      addToCart({
+        ...selectedItem,
+      });
+      closeModal();
+    }
+  };
+
+  const handleAddVariantToCart = () => {
+    if (selectedItem && selectedVariant) {
+      addToCart({
+        ...selectedItem,
+        selectedVariant,
+      });
+      closeModal();
+    }
+  };
+
+  const closeVariantModal = () => {
+    setVariantModalOpen(true);
+  };
 
   return (
     <div>
       <ul className={css.productlist}>
         {items.map((item) => (
-            <li key={item ? item.id : 'default'} className={css.productitem} onClick={() => openModal(item)}>
-            {item && <Products
-              url={item.url}
-              title={item.title}
-              price={item.price}
-              quontity={item.quantity}
-                />}
+          <li key={item ? item.id : 'default'} className={css.productitem} onClick={() => openModal(item)}>
+            {item && (
+              <Products
+                url={item.url}
+                title={item.title}
+                price={item.price}
+                quantity={item.quantity}
+              />
+            )}
           </li>
         ))}
       </ul>
 
       {selectedItem && (
         <div className={css.modal}>
-            <div className={css.modalcontent}>
+          <div className={css.modalcontent}>
             <img src={selectedItem.url} alt={selectedItem.title} />
             <p>{selectedItem.title}</p>
             <p>{selectedItem.description}</p>
             <p>{selectedItem.price}</p>
             <p>{selectedItem.quantity}</p>
-             <button onClick={() => handleAddToCart(selectedItem)}>Добавить в корзину </button>
-            <button onClick={closeModal}>Закрыть</button>
+             <button onClick={closeModal}>Закрыть</button>
+            <button onClick={handleAddToCart}>Добавить в корзину</button>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography>Аромати</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  {selectedItem.variants && (
+                    <span>
+                      {selectedItem.variants.map((variant) => (
+                        <span key={variant.id}>
+                          <button onClick={() => openModal(variant)}>
+                            <p>{variant.title}</p>
+                          </button>
+                        </span>
+                      ))}
+                    </span>
+                  )}
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+        </div>
+      )}
+
+      {selectedVariant && variantModalOpen && (
+        <div className={css.variantModal}>
+          <div className={css.modalcontent}>
+            <img src={selectedVariant.url} alt={selectedVariant.title} />
+            <p>{selectedVariant.title}</p>
+            <p>{selectedVariant.description}</p>
+            <button onClick={handleAddVariantToCart}>Добавить в корзину</button>
+            <button onClick={closeVariantModal}>Закрыть</button>
           </div>
         </div>
       )}
     </div>
   );
 };
-
 
 export default ProductsList;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,30 +3,52 @@ import PropTypes from 'prop-types';
 import css from './Products.module.css';
 
 const Product = ({
-        url,
-        title,
-        description,
-        price,
-        quantity
-   }) => {
-    return (
-        <div className={css.product}>
-            <img className={css.productimg} src={url} alt={1} width='100px' />
-            <p className={css.productTitle}> {title} </p>
-            <p className={css.productdesc}>{description} </p>
-            <p className={css.productprice}>{price} ₴ </p>
-            <p className={css.productquan}>{quantity < 10 ? 'заканчивается' : 'есть в наличии'}</p>
+  url,
+  title,
+  description,
+  price,
+  quantity,
+  variants,
+  onSelectVariant
+}) => {
+  const handleVariantClick = (variant) => {
+    onSelectVariant(variant);
+  };
+
+  return (
+    <div className={css.product}>
+      <img className={css.productimg} src={url} alt={title} width='100px' />
+      <p className={css.productTitle}>{title}</p>
+      <p className={css.productdesc}>{description}</p>
+      <p className={css.productprice}>{price} ₴</p>
+      <p className={css.productquan}>
+        {quantity < 10 ? 'заканчивается' : 'есть в наличии'}
+      </p>
+
+      {variants && variants.length > 0 && (
+        <div className={css.variants}>
+          <p>Выберите вариант:</p>
+          <ul>
+            {variants.map((variant) => (
+              <li key={variant.id} onClick={() => handleVariantClick(variant)}>
+                {variant.title}
+              </li>
+            ))}
+          </ul>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 Product.propTypes = {
-    url: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string,
-    price: PropTypes.number,
-    quantity:PropTypes.number,
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  price: PropTypes.number,
+  quantity: PropTypes.number,
+  variants: PropTypes.array,
+  onSelectVariant: PropTypes.func
+};
 
-}
-
-export default Product
+export default Product;
