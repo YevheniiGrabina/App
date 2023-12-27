42155d15379f6b211fe3d5b282bb9ed660fdf376
YevheniiGrabina <89870811+YevheniiGrabina@users.noreply.github.com>
2023-12-27 13:41:29 +0200

    fix

--- src/components/ProductList/ProductsList.jsx
+++ src/components/ProductList/ProductsList.jsx
@@ -4,19 +4,19 @@ import  css from'./ProductList.module.css'
 
 const ProductsList = ({ items, addToCart }) => {
   const [selectedItem, setSelectedItem] = useState(null);
-  const [selectedVariant, setSelectedVariant] = useState(null);
-  const [variantModalOpen, setVariantModalOpen] = useState(false);
+ 
+
 
   const openModal = (item) => {
     setSelectedItem(item);
-    setSelectedVariant(item.variants ? item.variants : null);
-    setVariantModalOpen(false); // Скрыть окно с вариантом при открытии основного окна
+   
+  
   };
 
   const closeModal = () => {
     setSelectedItem(null);
-    setSelectedVariant(null);
-    setVariantModalOpen(false);
+
+
   };
 
   const handleAddToCart = () => {
@@ -26,20 +26,6 @@ const ProductsList = ({ items, addToCart }) => {
       });
       closeModal();
     }
-  };
-
-  const handleAddVariantToCart = () => {
-    if (selectedItem && selectedVariant) {
-      addToCart({
-        ...selectedItem,
-        selectedVariant,
-      });
-      closeModal();
-    }
-  };
-
-  const closeVariantModal = () => {
-    setVariantModalOpen(true);
   };
 
   return (
@@ -62,24 +48,12 @@ const ProductsList = ({ items, addToCart }) => {
       {selectedItem && (
         <div className={css.modal}>
           <div className={css.modalcontent}>
-              <button className={css.closemodalbtn} onClick={closeModal}>Х</button>
+            <button className={css.closemodalbtn} onClick={closeModal}>X</button>
             <img className={css.modalimg} src={selectedItem.url} alt={selectedItem.title} />
             <p className={css.modaltitle}>{selectedItem.title}</p>
             <p className={css.modalprice}>Ціна:{selectedItem.price}₴</p>
             <p className={css.modaldesc}>{selectedItem.description}</p>
-            <button className={css.modalbtn} onClick={handleAddToCart}>ДОДАТИ В КОШИК</button>
-          </div>
-        </div>
-      )}
-
-      {selectedVariant && variantModalOpen && (
-        <div className={css.variantModal}>
-          <div className={css.modalcontent}>
-            <img src={selectedVariant.url} alt={selectedVariant.title} />
-            <p>{selectedVariant.title}</p>
-            <p>{selectedVariant.description}</p>
-            <button onClick={handleAddVariantToCart}>Добавить в корзину</button>
-            <button onClick={closeVariantModal}>Х</button>
+             <button className={css.modalbtn} onClick={handleAddToCart}>Добавить в корзину</button>
           </div>
         </div>
       )}
